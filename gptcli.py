8c884a66ec4d8b7e2351a7f1a25e81393fcbfc42
Jim Darby <jimd@nowhere-fsajlfa.com>
2023-03-20 01:05:22 +0900

    implement -download and -summarize

--- gptcli.py
+++ gptcli.py
@@ -4,6 +4,8 @@ import os
 import json
 import argparse
 import openai
+import tiktoken
+import datetime
 from typing import List
 
 from rich.console import Console
@@ -15,6 +17,9 @@ try:
     import readline
 except ImportError:
     pass
+
+now = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+log_file = f"chatlog-{now}.md"
 
 class Config:
     sep = Markdown("---")
@@ -53,6 +58,10 @@ class Config:
     @property
     def proxy(self):
         return self.cfg.get("proxy", "")
+
+    @property
+    def threshold(self):
+        return self.cfg.get("threshold", 3200)
 
 c = Console()
 kConfig = Config()
@@ -108,10 +117,29 @@ def query_openai_stream(data: dict):
     c.print(Config.sep)
     return answer
 
+def num_tokens_from_messages(messages, model="gpt-3.5-turbo"):
+    """Returns the number of tokens used by a list of messages."""
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        encoding = tiktoken.get_encoding("cl100k_base")
+    if model == "gpt-3.5-turbo-0301" or model == "gpt-3.5-turbo":  # note: future models may deviate from this
+        num_tokens = 0
+        for message in messages:
+            num_tokens += 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
+            for key, value in message.items():
+                num_tokens += len(encoding.encode(value))
+                if key == "name":  # if there's a name, the role is omitted
+                    num_tokens += -1  # role is always required and always 1 token
+        num_tokens += 2  # every reply is primed with <im_start>assistant
+        return num_tokens
+    else:
+        raise NotImplementedError(f"""num_tokens_from_messages() is not presently implemented for model {model}.
+See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens.""")
 
 class ChatConsole:
 
-    def __init__(self) -> None:
+    def __init__(self, hist=[]) -> None:
         parser = argparse.ArgumentParser("Input", add_help=False)
         parser.add_argument('-help', action='help', default=argparse.SUPPRESS, help="show this help message")
         parser.add_argument("-reset", action='store_true',
@@ -120,7 +148,10 @@ class ChatConsole:
                             help="exit console")
         parser.add_argument("-multiline", action='store_true',
                             help="input multiple lines, end with ctrl-d(Linux/macOS) or ctrl-z(Windows). cancel with ctrl-c")
+        parser.add_argument("-download", action="store_true", help="download chat log as markdown file")
+        parser.add_argument("-summarize", action="store_true", help="summarize the conversation")
         self.parser = parser
+        self.hist = hist
         try:
             self.init_readline([opt for action in parser._actions for opt in action.option_strings])
         except Exception as e:
@@ -157,6 +188,10 @@ class ChatConsole:
             c.print("Session cleared.")
         elif args.multiline:
             return self.read_multiline()
+        elif args.download:
+            self.download_chatlog()
+        elif args.summarize:
+            summarize_history()
         elif args.exit:
             raise EOFError
         else:
@@ -176,6 +211,32 @@ class ChatConsole:
             contents.append(line)
         return "\n".join(contents)
 
+    def download_chatlog(self):
+        filename = f"chatlog-{datetime.datetime.now().strftime('%m%d%y-%H%M%S')}.md"
+        with open(filename, "w") as f:
+            f.write(f"# Chat Log\n\n")
+            for msg in self.hist:
+                if msg['role'] == 'user':
+                    f.write(f"## User:\n\n{msg['content']}\n\n")
+                else:
+                    f.write(f"## Bot:\n\n{msg['content']}\n\n")
+        c.print(f"Chat log saved to [bold green]{filename}[/] in markdown format.")
+
+    def summarize_history(self):
+        self.download_chatlog()
+        #user_messages = [m for m in self.hist if m["role"] == "user"]
+        #assistant_messages = [m for m in self.hist if m["role"] == "assistant"]
+        summarize_content = "Please help me summarize the above conversation in English, to reduce the number of words while ensuring the quality of the conversation. If the conversation contains unethical or inappropriate expressions, please summarize them to milder expressions. It is necessary to summarize the conversation from beginning to end in a way that covers all the main points, and it is acceptable to have a few more tokens to do so. Please do not include this sentence in your summary."
+        summarize_messages = self.hist[4:-6] + [{"role": "user", "content": summarize_content}]
+        try:
+            if kConfig.stream:
+                summary = query_openai_stream(summarize_messages)
+            else:
+                summary = query_openai(summarize_messages)
+        except Exception as e:
+            c.print(e)
+            return
+        self.hist[4:-6] = [{"role": "assistant", "content": summary}]
 
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -193,7 +254,7 @@ def main():
     c.print(f"Stream mode: {kConfig.stream}")
 
     hist = kConfig.history # just alias
-    chat = ChatConsole()
+    chat = ChatConsole(hist=hist)
     while True:
         try:
             content = chat.parse_input().strip()
@@ -214,6 +275,8 @@ def main():
             hist.pop()
         elif kConfig.response:
             hist.append({"role": "assistant", "content": answer})
+        if num_tokens_from_messages(hist, kConfig.model) >= kConfig.threshold:
+            chat.summarize_history()
 
 
 if __name__ == '__main__':
